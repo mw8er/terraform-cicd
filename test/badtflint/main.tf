67bf2868814ac2777bf8220eddc56e57fcea1a1b
Michael Wachter <49635794+mw8er@users.noreply.github.com>
2026-02-22 14:15:24 +0100

    Refactor code quality checks and update scripts (#3)
    * Refactor code quality checks and update scripts
    
    - Rename check.sh to check-quality.sh and update references
    - Enhance functions.sh with improved naming conventions for functions
    - Add new test cases for code quality checks
    - Update .gitignore to include additional JSON files
    - Modify README.md to document new code quality check commands
    
    * Enhance tfplan checks with trivy for medium and high severity vulnerabilities
    
    * Add Trivy configuration and update functions for quality checks
    
    * Add initial Terraform configuration and environment setup
    
    * Enhance CI workflow by adding experimental Nix features and enabling cache for devbox installation
    
    * Update Nix installer action to use the latest main branch and enable experimental features

---
+++ test/badtflint/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "local" {
+  # Configuration options
+}
+
+variable "test" {
+  type = string
+}
+
+resource "local_file" "foo" {
+  content  = format("%s / %s", var.test, timestamp())
+  filename = "${path.module}/foo.bar"
+}
---
+++ test/badtrivy/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_version = "~> 1.0"
+  required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.0"
+    }
+  }
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "storageaccountname"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
